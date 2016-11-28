4543fd27e6f6e5fd2bfe3b475c57347a9f7a8adc
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2016-11-28 14:15:01 +0100

    Readd config and stuff

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  if (!$("#contents").length)
+    return true;
+
+  $(".post-content h2, .post-content h3").each(function(i) {
+    $(this).after("<a href=#contents>Back to the top</a>").next()
+     .css("font-size", "0.8em")
+     .css("padding", "2px 4px")
+     .css("margin-bottom", "0")
+     .css("text-align", "center")
+     .css("vertical-align", "middle")
+     .css("-ms-touch-action", "manipulation")
+     .css("touch-action", "manipulation")
+     .css("cursor", "pointer")
+     .css("-webkit-user-select", "none")
+     .css("-moz-user-select", "none")
+     .css("-ms-user-select", "none")
+     .css("user-select", "none")
+     .css("border", "1px solid transparent")
+     .css("border-radius", "4px")
+     .css("color", "#fff")
+     .css("background-color", "rgba(0,30,180,0.5)")
+     .hover(
+       function() {
+         $(this).css("background-color", "rgba(0,30,180,1)")
+                .css("text-decoration", "none");
+       },
+       function() {
+         $(this).css("background-color", "rgba(0,30,180,0.5)");
+       });
+    });
+});
