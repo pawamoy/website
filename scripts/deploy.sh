dcae8ce96926360d517635dbb4f792362a2e3401
Timothée Mazzucotelli <pawamoy@pm.me>
2023-04-27 21:58:39 +0200

    Try to fix deploy script again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,14 +6,14 @@ git remote add gh-pages git@github.com:pawamoy/pawamoy.github.io &>/dev/null
 # reset publish branch, switch to it
 # this is to avoid a build commit on master
 git branch -D publish &>/dev/null
-git switch -c publish
+git switch -c publish -t gh-pages/master
 
 # build and publish
 . .venv/bin/activate
 mkdocs gh-deploy \
   --force --no-history --ignore-version \
   --remote-name gh-pages \
-  --remote-branch master
+  --remote-branch publish
 
 # switch back to master branch
 git switch master
