391e26d86c408cca746f856d4ea69c085f6e0e03
Timothée Mazzucotelli <pawamoy@pm.me>
2023-04-27 21:48:17 +0200

    Fix deploy script to avoid messing up master branch

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,19 @@
 #!/usr/bin/env bash
-. .venv/bin/activate
+
+# add remote
 git remote add gh-pages git@github.com:pawamoy/pawamoy.github.io &>/dev/null
-mkdocs gh-deploy --force \
+
+# reset publish branch, switch to it
+# this is to avoid a build commit on master
+git branch -D publish &>/dev/null
+git switch -c publish
+
+# build and publish
+. .venv/bin/activate
+mkdocs gh-deploy \
+  --force --no-history --ignore-version \
   --remote-name gh-pages \
   --remote-branch master
-# git reset --hard HEAD
+
+# switch back to master branch
+git switch master
