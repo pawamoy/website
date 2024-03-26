9dc85b6de6618053aeab939dc5fe216fe68bed98
Timothée Mazzucotelli <dev@pawamoy.fr>
2024-03-26 10:56:25 +0100

    Don't rewrite history when deploying

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git remote add pawamoy.github.io git@github.com:pawamoy/pawamoy.github.io &>/dev/null
 .venv/bin/mkdocs gh-deploy \
-  --force --no-history --ignore-version \
+  --force --ignore-version \
   --remote-name pawamoy.github.io \
   --remote-branch gh-pages
